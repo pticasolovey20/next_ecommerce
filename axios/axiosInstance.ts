e96d83477aa0746d419a55d7723d9256a0f52fe1
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-07-26 14:47:19 +0300

    updated axios instance config (base url)

--- axios/axiosInstance.ts
+++ axios/axiosInstance.ts
@@ -1,9 +1,13 @@
+import { getBaseURL } from "@/helpers/getBaseURL";
 import axios from "axios";
 
-const BASE_URL = process.env.BASE_URL || "http://localhost:3000";
+const BASE_URL = getBaseURL();
 
 const axiosInstance = axios.create({
   baseURL: BASE_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export default axiosInstance;
