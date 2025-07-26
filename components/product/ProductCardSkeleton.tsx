9167da3d154853bb144f46b0d953291f78553aad
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-07-26 15:18:27 +0300

    updated product card and skeleton styles

--- components/product/ProductCardSkeleton.tsx
+++ components/product/ProductCardSkeleton.tsx
@@ -1,13 +1,20 @@
+import { classNames } from "@/utils/classNames";
+
 const ProductCardSkeleton = () => {
   return (
-    <div className="relative w-full rounded-sm bg-muted shadow-lg cursor-pointer overflow-hidden">
-      <div className="relative w-full aspect-square bg-muted-foreground" />
+    <div
+      className={classNames(
+        "relative w-full",
+        "rounded-md border-2 border-muted-foreground",
+        "shadow-lg bg-muted cursor-pointer overflow-hidden"
+      )}
+    >
+      <div className="relative w-full aspect-square bg-muted" />
 
-      <div className="p-4">
-        <div className="w-full h-6 rounded-sm bg-muted-foreground" />
-        <div className="w-full h-6 mt-2 rounded-sm bg-muted-foreground" />
-
-        <div className="w-[100px] h-7 mt-4 rounded-sm bg-muted-foreground" />
+      <div className="p-4 border-t-2 border-muted-foreground bg-background">
+        <div className="w-full h-6 rounded-sm border-2 border-muted-foreground bg-muted" />
+        <div className="w-full h-6 mt-2 rounded-sm border-2 border-muted-foreground bg-muted" />
+        <div className="w-[100px] h-7 mt-4 rounded-sm border-2 border-muted-foreground bg-muted" />
       </div>
     </div>
   );
