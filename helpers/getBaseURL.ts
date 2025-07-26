1f7d68dd5ac4d68d21464ebd118c64d480132d61
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-07-26 14:59:27 +0300

    fixed getBaseURL func

--- helpers/getBaseURL.ts
+++ helpers/getBaseURL.ts
@@ -1,10 +1,6 @@
 export const getBaseURL = () => {
   if (process.env.NODE_ENV === "production") {
-    return process.env.BASE_URL || "https://your-production-api.com";
-  }
-
-  if (process.env.NODE_ENV === "development") {
-    return process.env.BASE_URL || "https://your-staging-api.com";
+    return process.env.BASE_URL;
   }
 
   return process.env.BASE_URL || "http://localhost:3000";
