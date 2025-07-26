2f1615a8fddc4cec761deabd520ea53a3eeee4e0
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-07-26 21:31:48 +0300

    added aria label to the icon buttons, fixed product card responsivenes

--- components/product/ProductCard.tsx
+++ components/product/ProductCard.tsx
@@ -12,34 +12,37 @@ interface ProductCardProps {
 
 const ProductCard = ({ product }: ProductCardProps) => {
   return (
-    <Link href={`/products/${product.id}`}>
-      <div
-        className={classNames(
-          "relative w-full",
-          "rounded-md border-2 border-muted-foreground",
-          "shadow-lg bg-muted cursor-pointer overflow-hidden"
-        )}
-      >
-        <div className="relative w-full aspect-square bg-background" />
-
-        <IconButton className="absolute top-4 right-4 w-8 h-8 hover:bg-muted">
-          <IoMdHeartEmpty className="w-full h-full" />
-        </IconButton>
+    <div
+      className={classNames(
+        "relative w-full",
+        "rounded-md border-2 border-muted-foreground",
+        "shadow-lg bg-muted overflow-hidden"
+      )}
+    >
+      <Link href={`/products/${product.id}`}>
+        <div className="relative w-full aspect-square bg-background hover:bg-muted/50 transition-colors" />
 
         <div className="p-4 border-t-2 border-muted-foreground">
           <h2 className="font-semibold">{product.title}</h2>
           <p className="w-full truncate text-muted-foreground">{product.description}</p>
-
-          <div className="flex items-center justify-between mt-4">
-            <span className="text-xl font-semibold">${product.price}</span>
-
-            <IconButton className="w-9 h-9 p-2 hover:bg-background">
-              <RiShoppingCartLine className="w-full h-full" />
-            </IconButton>
-          </div>
+          <span className="text-xl font-semibold mt-4 block">${product.price}</span>
         </div>
-      </div>
-    </Link>
+      </Link>
+
+      <IconButton
+        ariaLabel="Add to favorite"
+        className="absolute top-4 right-4 w-8 h-8 hover:bg-muted z-10"
+      >
+        <IoMdHeartEmpty className="w-full h-full" />
+      </IconButton>
+
+      <IconButton
+        ariaLabel="Add to cart"
+        className="absolute bottom-4 right-4 w-9 h-9 p-2 hover:bg-background z-10"
+      >
+        <RiShoppingCartLine className="w-full h-full" />
+      </IconButton>
+    </div>
   );
 };
 
--- ui/IconButton.tsx
+++ ui/IconButton.tsx
@@ -3,14 +3,16 @@ import { classNames } from "@/utils/classNames";
 
 interface IconButtonProps {
   onClick?: () => void;
+  ariaLabel: string;
   className?: string;
   children: ReactNode;
 }
 
-const IconButton = ({ onClick, className, children }: IconButtonProps) => {
+const IconButton = ({ onClick, ariaLabel, className, children }: IconButtonProps) => {
   return (
     <button
       onClick={onClick}
+      aria-label={ariaLabel}
       className={classNames(
         "flex items-center justify-center",
         "p-1 rounded-md cursor-pointer hover:bg-gray-300",
