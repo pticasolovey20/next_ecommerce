c0af95be53ec4bfb341e6bb75e3ae088124d01c4
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-07-26 15:04:02 +0300

    updated banner styles

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,6 +1,14 @@
+import { classNames } from "@/utils/classNames";
+
 const Banner = () => {
   return (
-    <div className="h-[300px] flex items-center justify-center p-4 my-12 rounded-xl shadow-md bg-muted-foreground">
+    <div
+      className={classNames(
+        "h-[300px] flex items-center justify-center",
+        "rounded-xl border-2 border-foreground",
+        "p-4 my-12 shadow-md bg-muted"
+      )}
+    >
       <h1 className="text-3xl font-bold">Banner</h1>
     </div>
   );
