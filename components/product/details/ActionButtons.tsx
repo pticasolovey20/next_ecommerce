a3e4763f76fd169321d6d22680adeb8273920278
Dmytro Soloviev <dy.solovyov.work@gmail.com>
2025-08-05 13:22:54 +0300

    updated ui button styles

--- components/product/details/ActionButtons.tsx
+++ components/product/details/ActionButtons.tsx
@@ -1,29 +1,13 @@
-import { classNames } from "@/utils/classNames";
-
 import Button from "@/ui/Button";
 
 const ActionButtons = () => {
   return (
     <div className="flex flex-col sm:flex-row gap-4">
-      <Button
-        size="lg"
-        variant="primary"
-        className={classNames(
-          "w-full hover-lift",
-          "font-semibold text-lg text-gray-700 uppercase tracking-wide"
-        )}
-      >
+      <Button size="lg" variant="primary" className="w-full hover-lift font-semibold uppercase">
         buy
       </Button>
 
-      <Button
-        size="lg"
-        variant="secondary"
-        className={classNames(
-          "w-full hover-lift",
-          "font-semibold text-lg text-gray-700 uppercase tracking-wide"
-        )}
-      >
+      <Button size="lg" variant="secondary" className="w-full hover-lift font-semibold uppercase">
         add to cart
       </Button>
     </div>
